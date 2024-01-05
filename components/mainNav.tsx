7a958a2ebcc5705cec76c6693dce7f797fffe08b
Kim Tuan Doan <doantuan21101999@gmail.com>
2024-01-05 10:33:35 +0700

    Add layout and common components

---
+++ components/mainNav.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Menu } from './menu';
+import { cn } from '@/lib/utils';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { CommandDialog } from './command';
+
+const MainNav = () => {
+  const [isOpenLogin, setIsOpenLogin] = useState<boolean>(false);
+  const mainNav = [
+    { title: 'Home', href: '/' },
+    { title: 'History', href: '/history' },
+  ];
+
+  const handleClickLogin = () => {
+    setIsOpenLogin(true);
+  };
+  return (
+    <header className="container z-40 bg-background">
+      <div className="flex h-20 items-center justify-between py-6">
+        <Menu items={mainNav} />
+        <nav>
+          <Button
+            className={cn(
+              buttonVariants({ variant: 'secondary', size: 'sm' }),
+              'px-4',
+            )}
+            onClick={handleClickLogin}
+          >
+            Login
+          </Button>
+        </nav>
+      </div>
+      <CommandDialog
+        open={isOpenLogin}
+        onOpenChange={() => setIsOpenLogin(false)}
+      >
+        <div>login</div>
+      </CommandDialog>
+    </header>
+  );
+};
+
+export default MainNav;
